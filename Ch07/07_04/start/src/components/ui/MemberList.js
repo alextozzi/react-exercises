68ff9cb37c268fed5717294da8a395add643062f
Alexandre Tozzi <alexandre@pau.be>
2017-11-22 13:08:02 +0100

    finishing tutorial

--- Ch07/07_04/start/src/components/ui/MemberList.js
+++ Ch07/07_04/start/src/components/ui/MemberList.js
@@ -8,8 +8,11 @@ class MemberList extends Component {
         super(props)
         this.state = {
             members: [],
-            loading: false
+            loading: false,
+            administrators: []
         }
+        this.makeAdmin = this.makeAdmin.bind(this)
+        this.removeAdmin = this.removeAdmin.bind(this)
     }
 
     componentDidMount() {
@@ -21,6 +24,20 @@ class MemberList extends Component {
                 members,
                 loading: false
             }))
+    }
+    makeAdmin(email) {
+        const administrators = [
+            ...this.state.administrators,
+            email
+        ]
+        this.setState({administrators})
+
+    }
+    removeAdmin(email) {
+        const administrators = this.state.administrators.filter(
+            adminEmail => adminEmail !== email
+        )
+        this.setState({administrators})
     }
 
     render() {
@@ -37,10 +54,15 @@ class MemberList extends Component {
                 {(members.length) ?
                    members.map(
                 	(member, i) => 
-                		<Member key={i} 
+                		<Member key={i}
+                                admin={this.state.administrators.some(
+                                    adminEmail => adminEmail === member.email
+                                    )}
                                 name={member.name.first + ' ' + member.name.last} 
                                 email={member.email}
-                                thumbnail={member.picture.thumbnail}/>
+                                thumbnail={member.picture.thumbnail}
+                                makeAdmin={this.makeAdmin}
+                                removeAdmin={this.removeAdmin}/>
                 	 ):
                    <span>Currently 0 Members </span>
                }
